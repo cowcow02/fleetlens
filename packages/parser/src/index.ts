5e9d71c84257320ddc278645e084bedca2e1d187
cowcow02 <makchamhim72@gmail.com>
2026-04-15 13:07:43 +0800

    feat(parser): add groupByTeam() for lead/member correlation

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -9,3 +9,5 @@ export * from "./types.js";
 export * from "./parser.js";
 export * from "./presentation.js";
 export * from "./analytics.js";
+export { groupByTeam } from "./team.js";
+export type { TeamView, TeamMessage } from "./team.js";
